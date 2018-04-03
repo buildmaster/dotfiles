da769f51ba03e296f34c89068c7988caeb5be114
Owen Evans <owen@bgeek.net>
2018-04-03 12:13:25 +1200

    add lastpass

--- setup/init_machine.sh
+++ setup/init_machine.sh
@@ -64,6 +64,7 @@ brew cask install caskroom/drivers/sonos
 brew cask install sequel-pro
 brew cask install gpg-suite
 brew cask install viscosity
+brew cask install lastpass
 # cask quick look plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo
 
